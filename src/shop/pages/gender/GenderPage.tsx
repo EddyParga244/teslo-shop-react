dfb56191277b004288b9b0c4674126f51c21ff92
Eduardo Parga <eddypargavela@gmail.com>
2026-03-12 13:04:26 -0600

    Initial commit

---
+++ src/shop/pages/gender/GenderPage.tsx
@@ -0,0 +1,25 @@
+import { CustomPagination } from "@/components/custom/CustomPagination";
+import { CustomJumbotron } from "@/shop/components/CustomJumbotron";
+import { ProductsGrid } from "@/shop/components/ProductsGrid";
+import { useProducts } from "@/shop/hooks/useProducts";
+import { useParams } from "react-router";
+
+export const GenderPage = () => {
+  const { gender } = useParams();
+  const { data } = useProducts();
+
+  const genderLabel =
+    gender === "men" ? "Hombres" : gender === "women" ? "Mujeres" : "Niños";
+
+  return (
+    <>
+      <CustomJumbotron
+        title={`Productos para ${genderLabel}`}
+      ></CustomJumbotron>
+
+      <ProductsGrid products={data?.products || []}></ProductsGrid>
+
+      <CustomPagination totalPages={data?.pages || 1}></CustomPagination>
+    </>
+  );
+};
---
+++ src/shop/pages/home/HomePage.tsx
@@ -0,0 +1,16 @@
+import { CustomPagination } from "@/components/custom/CustomPagination";
+import { CustomJumbotron } from "@/shop/components/CustomJumbotron";
+import { ProductsGrid } from "@/shop/components/ProductsGrid";
+import { useProducts } from "@/shop/hooks/useProducts";
+
+export const HomePage = () => {
+  const { data } = useProducts();
+
+  return (
+    <>
+      <CustomJumbotron title="Todos los productos"></CustomJumbotron>
+      <ProductsGrid products={data?.products || []}></ProductsGrid>
+      <CustomPagination totalPages={data?.pages || 0}></CustomPagination>
+    </>
+  );
+};
